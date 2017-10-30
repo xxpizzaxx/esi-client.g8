e89c8701b7ca897f0f624eac642375c9a96f7733
Andi Miller <andi@andimiller.net>
2017-10-30 16:59:05 +0000

    scalafmt

--- src/main/g8/src/main/scala/$package$/Client.scala
+++ src/main/g8/src/main/scala/$package$/Client.scala
@@ -16,28 +16,43 @@ object Client extends App {
 
   // let's request the API's status
   val statusRequest = status.getStatus()
-  val statusRequestIO = statusRequest.run(esiClient).map(_.right.get.asJson.spaces2)
+  val statusRequestIO =
+    statusRequest.run(esiClient).map(_.right.get.asJson.spaces2)
   println(statusRequestIO.unsafeRunSync)
 
   // let's compose something more complex
   // how about the top 10 ratting systems augmented with their map data
   val statsRequest = for {
     // let's get the systems with the top 10 NPC kills in the last hour
-    kills   <- universe.getUniverseSystemKills().run(esiClient).map(_.right.get.sortBy(0 -_.npc_kills).take(10))
+    kills <- universe
+      .getUniverseSystemKills()
+      .run(esiClient)
+      .map(_.right.get.sortBy(0 - _.npc_kills).take(10))
     // and and let's look up their map data
-    results <- kills.map{ stats =>
+    results <- kills.map { stats =>
       for {
-        system        <- universe.getUniverseSystemsSystemId(stats.system_id).run(esiClient).map(_.right.get)
-        constellation <- universe.getUniverseConstellationsConstellationId(system.constellation_id).run(esiClient).map(_.right.get)
-        region        <- universe.getUniverseRegionsRegionId(constellation.region_id).run(esiClient).map(_.right.get)
+        system <- universe
+          .getUniverseSystemsSystemId(stats.system_id)
+          .run(esiClient)
+          .map(_.right.get)
+        constellation <- universe
+          .getUniverseConstellationsConstellationId(system.constellation_id)
+          .run(esiClient)
+          .map(_.right.get)
+        region <- universe
+          .getUniverseRegionsRegionId(constellation.region_id)
+          .run(esiClient)
+          .map(_.right.get)
       } yield (stats, system, constellation, region)
     }.sequence
   } yield (results)
   // and let's print them out nicely
-  val statsText = statsRequest.unsafeRunSync.map{ case (stats, system, constellation, region) =>
-    s"${system.name} in ${constellation.name} in ${region.name} had ${stats.npc_kills} npc kills in the last hour"
-  }.mkString("\n")
+  val statsText = statsRequest.unsafeRunSync
+    .map {
+      case (stats, system, constellation, region) =>
+        s"${system.name} in ${constellation.name} in ${region.name} had ${stats.npc_kills} npc kills in the last hour"
+    }
+    .mkString("\n")
   println(statsText)
-
 
 }
