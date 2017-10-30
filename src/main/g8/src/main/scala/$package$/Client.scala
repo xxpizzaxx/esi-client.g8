e800dd0ed136d56c54f145fcd6b9835b6a208f89
Andi Miller <andi@andimiller.net>
2017-10-30 17:00:54 +0000

    scalafmt2

--- src/main/g8/src/main/scala/$package$/Client.scala
+++ src/main/g8/src/main/scala/$package$/Client.scala
@@ -12,7 +12,7 @@ import cats.syntax._, cats.implicits._
 
 object Client extends App {
   val httpClient = PooledHttp1Client[IO]()
-  val esiClient = new EsiClient[IO]("my esi client", httpClient.toHttpService)
+  val esiClient  = new EsiClient[IO]("my esi client", httpClient.toHttpService)
 
   // let's request the API's status
   val statusRequest = status.getStatus()
@@ -25,26 +25,26 @@ object Client extends App {
   val statsRequest = for {
     // let's get the systems with the top 10 NPC kills in the last hour
     kills <- universe
-      .getUniverseSystemKills()
-      .run(esiClient)
-      .map(_.right.get.sortBy(0 - _.npc_kills).take(10))
+              .getUniverseSystemKills()
+              .run(esiClient)
+              .map(_.right.get.sortBy(0 - _.npc_kills).take(10))
     // and and let's look up their map data
     results <- kills.map { stats =>
-      for {
-        system <- universe
-          .getUniverseSystemsSystemId(stats.system_id)
-          .run(esiClient)
-          .map(_.right.get)
-        constellation <- universe
-          .getUniverseConstellationsConstellationId(system.constellation_id)
-          .run(esiClient)
-          .map(_.right.get)
-        region <- universe
-          .getUniverseRegionsRegionId(constellation.region_id)
-          .run(esiClient)
-          .map(_.right.get)
-      } yield (stats, system, constellation, region)
-    }.sequence
+                for {
+                  system <- universe
+                             .getUniverseSystemsSystemId(stats.system_id)
+                             .run(esiClient)
+                             .map(_.right.get)
+                  constellation <- universe
+                                    .getUniverseConstellationsConstellationId(system.constellation_id)
+                                    .run(esiClient)
+                                    .map(_.right.get)
+                  region <- universe
+                             .getUniverseRegionsRegionId(constellation.region_id)
+                             .run(esiClient)
+                             .map(_.right.get)
+                } yield (stats, system, constellation, region)
+              }.sequence
   } yield (results)
   // and let's print them out nicely
   val statsText = statsRequest.unsafeRunSync
