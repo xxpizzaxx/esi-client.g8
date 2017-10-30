f5055bb40f6a23005f3751b790c404c677756eae
Andi Miller <andi@andimiller.net>
2017-10-30 14:39:05 +0000

    make it push the version to git

--- test.sh
+++ test.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
+set -e
+
 VERSION="`cat esi-version/version`"
 sbt new file://./esi-client.g8 --name=democlient --organization=com.example --scala_version=2.11.8 --esi_client_version=$VERSION
 cd democlient
 sbt compile
+echo "it worked! let's bump the default"
+cd ../esi-client.g8
+sed -i 's/^esi_client_version = .*i$/esi_client_version = $VERSION/g' src/main/g8/default.properties
+git config --global user.name "Concourse CI"
+git config --global user.email "ci@pizza.moe"
+git commit -m "bumping default version to $VERSION"
