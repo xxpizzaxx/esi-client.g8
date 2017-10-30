d4767ff0010b553d7ca69809f4d0cd8904c189f2
Andi Miller <andi@andimiller.net>
2017-10-30 02:14:06 +0000

    add an advanced example

--- src/main/g8/src/main/scala/$package$/Client.scala
+++ src/main/g8/src/main/scala/$package$/Client.scala
@@ -8,6 +8,7 @@ import cats.effect._
 import io.circe._, io.circe.syntax._, io.circe.generic.auto._
 import io.circe.java8.time._
 import cats.syntax.either._
+import cats.syntax._, cats.implicits._
 
 object Client extends App {
   val httpClient = PooledHttp1Client[IO]()
@@ -15,6 +16,29 @@ object Client extends App {
 
   // let's request the API's status
   val statusRequest = StatusApi.getStatus()
-  val statusRequestIO = esiClient.run(statusRequest).map(_.map(_.asJson.spaces2))
+  val statusRequestIO = esiClient.run(statusRequest).map(_.right.get.asJson.spaces2)
   println(statusRequestIO.unsafeRunSync)
+
+  // let's compose something more complex
+  // how about ratting data augmented with system names?
+
+  val statsRequest = for {
+    // let's get the systems with the top 10 NPC kills in the last hour
+    kills   <- esiClient.run(UniverseApi.getUniverseSystemKills()).map(_.right.get.sortBy(0 -_.npc_kills).take(10))
+    // and and let's look up their map data
+    results <- kills.map{ stats =>
+      for {
+        system        <- esiClient.run(UniverseApi.getUniverseSystemsSystemId(stats.system_id)).map(_.right.get)
+        constellation <- esiClient.run(UniverseApi.getUniverseConstellationsConstellationId(system.constellation_id)).map(_.right.get)
+        region        <-esiClient.run(UniverseApi.getUniverseRegionsRegionId(constellation.region_id)).map(_.right.get)
+      } yield (stats, system, constellation, region)
+    }.sequence
+  } yield (results)
+  // and let's print them out nicely
+  val statsText = statsRequest.unsafeRunSync.map{ case (stats, system, constellation, region) =>
+    s"${system.name} in ${constellation.name} in ${region.name} had ${stats.npc_kills} npc kills in the last hour"
+  }.mkString("\n")
+  println(statsText)
+
+
 }
