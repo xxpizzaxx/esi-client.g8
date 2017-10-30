8dd17ddd8942f87e75dadf3d66f173341fcc0765
Andi Miller <andi@andimiller.net>
2017-10-30 14:10:01 +0000

    add some checks for concourse

--- test.sh
+++ test.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+ls
+ls esi-version
 VERSION="`cat esi-version/version.sbt`"
 sbt new file://./esi-client.g8 --name=democlient --organization=com.example --scala_version=2.11.8 --esi_client_version=$VERSION
