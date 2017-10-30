fd8508f76409d264f9cbeb9f6e8cc6ed7fda666d
Andi Miller <andi@andimiller.net>
2017-10-30 14:18:05 +0000

    make it build

--- test.sh
+++ test.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 VERSION="`cat esi-version/version`"
 sbt new file://./esi-client.g8 --name=democlient --organization=com.example --scala_version=2.11.8 --esi_client_version=$VERSION
+cd democlient
+sbt compile
